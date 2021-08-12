44b41facd75bb175064e774b8d214839cdecd420
raddevus <1radeutsch1@gmail.com>
2021-08-12 16:41:14 -0400

    Added check on button to control turning on of main LED.

--- Test4809Pins.ino
+++ Test4809Pins.ino
@@ -1,16 +1,40 @@
 
 const byte MAIN_LED = 6;
+const byte MAIN_BUTTON = 3;
+boolean mainLedLast = LOW;
+boolean mainLedCurrent = LOW;
+boolean mainLedOn = false;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin MAIN_LED as an output.
   pinMode(MAIN_LED, OUTPUT);
+  pinMode(MAIN_BUTTON, INPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(MAIN_LED, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(MAIN_LED, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  checkButton(MAIN_LED, MAIN_BUTTON, mainLedLast, mainLedCurrent,mainLedOn);
+}
+
+void checkButton(int LED, int BUTTON,  bool &last,bool &current, bool &ledOn ){
+  current = debounce(last, BUTTON);              // Read debounced state
+  if (last == LOW && current == HIGH)    // If it was pressed…
+  {
+    ledOn = !ledOn;                                  // Toggle the LED value
+  }
+  last = current;                        // Reset button value
+  
+  digitalWrite(LED, ledOn);                          // Change the LED state
+}
+
+boolean debounce(boolean last,int BUTTON)
+{
+  boolean current = digitalRead(BUTTON);       // Read the button state
+  if (last != current)                         // If it's different…
+  {
+    delay(5);                                  // Wait 5ms
+    current = digitalRead(BUTTON);             // Read it again
+  }
+  return current;                              // Return the current value
 }
